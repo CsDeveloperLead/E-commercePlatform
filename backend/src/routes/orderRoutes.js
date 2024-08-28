5ccc7fc9fd1b227c3774a261d68f5402a88a4a8b
abc <abc@example.com>
2024-08-28 16:34:37 +0530

    commit

---
+++ backend/src/routes/orderRoutes.js
@@ -0,0 +1,12 @@
+// routes/orderRoutes.js
+
+const express = require('express');
+const { addOrderItems, getOrderById } = require('../controllers/orderController');
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post('/', protect, addOrderItems);
+router.get('/:id', protect, getOrderById);
+
+module.exports = router;
